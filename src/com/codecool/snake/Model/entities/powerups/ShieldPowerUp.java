41fc21f2bcbbec1bfb22bb0bddf8e3bf604b533d
WojciechRekosiewicz <wojtek24a@gmail.com>
2018-09-04 13:32:19 +0200

    delete unused imports in medkit class

--- src/com/codecool/snake/Model/entities/powerups/ShieldPowerUp.java
+++ src/com/codecool/snake/Model/entities/powerups/ShieldPowerUp.java
@@ -1,4 +1,26 @@
 package com.codecool.snake.Model.entities.powerups;
 
+import com.codecool.snake.Model.Globals;
+import com.codecool.snake.Model.Utils;
+import com.codecool.snake.Model.entities.snakes.SnakeHead;
+import javafx.scene.layout.Pane;
+import com.codecool.snake.View.GameEntity;
+import com.codecool.snake.Model.Globals;
+import com.codecool.snake.Model.entities.Animatable;
+import com.codecool.snake.Model.Utils;
+import com.codecool.snake.Model.entities.Interactable;
+import com.codecool.snake.Model.entities.snakes.SnakeHead;
+import javafx.geometry.Point2D;
+import javafx.scene.layout.Pane;
+import com.codecool.snake.View.GameEntity;
+
+import com.codecool.snake.View.GameEntity;
+import com.codecool.snake.Model.Globals;
+import com.codecool.snake.Model.entities.Interactable;
+import com.codecool.snake.Model.entities.snakes.SnakeHead;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
 public class ShieldPowerUp {
 }
