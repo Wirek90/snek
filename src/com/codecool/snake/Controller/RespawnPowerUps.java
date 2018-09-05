9ebaffb9446ffe2dc8c7e8abf9a1b7b0028eed5e
WojciechRekosiewicz <wojtekrekosiewicz@poczta.fm>
2018-09-05 19:42:14 +0200

    added RespawnPowerUps class

---
+++ src/com/codecool/snake/Controller/RespawnPowerUps.java
@@ -0,0 +1,38 @@
+package com.codecool.snake.Controller;
+
+import com.codecool.snake.Model.entities.lasers.SimpleLaser;
+import com.codecool.snake.Model.entities.powerups.MedkitPowerUp;
+import com.codecool.snake.Model.entities.powerups.ShieldPowerUp;
+import com.codecool.snake.View.GameLoop;
+import com.codecool.snake.Model.Globals;
+import com.codecool.snake.Model.entities.enemies.SimpleEnemy;
+import com.codecool.snake.Model.entities.powerups.SimplePowerup;
+import com.codecool.snake.Model.entities.snakes.SnakeHead;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+public class RespawnPowerUps extends Pane {
+    SnakeHead snek;
+
+//    public void Game() {
+//        snek = new SnakeHead(this, 500, 500);
+//        new SimpleEnemy(this);
+//        new SimpleEnemy(this);
+//        new SimpleEnemy(this);
+//        new SimpleEnemy(this);
+//
+//        new SimplePowerup(this);
+//        new SimplePowerup(this);
+//        new SimplePowerup(this);
+//        new SimplePowerup(this);
+//
+//        new MedkitPowerUp(this);
+//        new MedkitPowerUp(this);
+//        new MedkitPowerUp(this);
+//        new MedkitPowerUp(this);
+//
+//        new ShieldPowerUp(this);
+//
+//    }
+
+}
